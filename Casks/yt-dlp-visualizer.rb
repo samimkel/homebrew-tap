6d9fab56c2a2a0a3f93f3833f7f739a9445a5151
Samim Kel <samimkel00@gmail.com>
2026-05-24 20:55:30 +0300

    Update sha256 checksum for yt-dlp-visualizer

--- Casks/yt-dlp-visualizer.rb
+++ Casks/yt-dlp-visualizer.rb
@@ -1,6 +1,6 @@
 cask "yt-dlp-visualizer" do
   version "1.0" # Bu uygulamanın versiyonu olarak kalabilir
-  sha256 "3a7b6403920c3ae2f6e0f123988dda20c89ea68d33bca2d3f06a65b755325616"
+  sha256 "5682f3f279dfef890317376367b632725df3c942262ab059b4cd0a3d24eb2b7a"
 
   # url kısmındaki v#{version} yerine doğrudan senin etiketin olan "App" yazıyoruz
   url "https://github.com/samimkel/YT-DLP-Visualizer/releases/tag/App/YT-DLP-Visualizer.zip"
