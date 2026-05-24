d95e59d821ebeb84d8c8c50760b5e781ea47cf17
Samim KEL <samimkel00@gmail.com>
2026-05-24 20:52:44 +0300

    this is the final one I promise

--- Casks/yt-dlp-visualizer.rb
+++ Casks/yt-dlp-visualizer.rb
@@ -3,7 +3,7 @@ cask "yt-dlp-visualizer" do
   sha256 "3a7b6403920c3ae2f6e0f123988dda20c89ea68d33bca2d3f06a65b755325616"
 
   # url kısmındaki v#{version} yerine doğrudan senin etiketin olan "App" yazıyoruz
-  url "https://github.com/samimkel/YT-DLP-Visualizer/releases/download/App/YT-DLP-Visualizer.zip"
+  url "https://github.com/samimkel/YT-DLP-Visualizer/releases/tag/App/YT-DLP-Visualizer.zip"
   name "YT-DLP Visualizer"
   desc "Minimalist macOS GUI companion for yt-dlp"
   homepage "https://github.com/samimkel/YT-DLP-Visualizer"
