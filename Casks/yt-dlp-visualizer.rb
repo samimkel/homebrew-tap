3db81b8ce49660e6b9a28e5845d0cedeb8fe2bef
Samim Kel <samimkel00@gmail.com>
2026-05-24 21:01:38 +0300

    Update download URL for yt-dlp-visualizer

--- Casks/yt-dlp-visualizer.rb
+++ Casks/yt-dlp-visualizer.rb
@@ -1,13 +1,15 @@
 cask "yt-dlp-visualizer" do
-  version "1.0" # Bu uygulamanın versiyonu olarak kalabilir
+  version "1.0"
   sha256 "5682f3f279dfef890317376367b632725df3c942262ab059b4cd0a3d24eb2b7a"
 
-  # url kısmındaki v#{version} yerine doğrudan senin etiketin olan "App" yazıyoruz
-  url "https://github.com/samimkel/YT-DLP-Visualizer/releases/tag/App/YT-DLP-Visualizer.zip"
+  # Linki senin attığın linkle birebir eşitledim
+  url "https://github.com/samimkel/YT-DLP-Visualizer/releases/download/App/YT-DLPVisualizer.zip"
   name "YT-DLP Visualizer"
   desc "Minimalist macOS GUI companion for yt-dlp"
   homepage "https://github.com/samimkel/YT-DLP-Visualizer"
 
+  # Uygulama isminin de zip içindekiyle aynı olması lazım
+  # Eğer zip içinde ismi farklıysa burayı o isimle düzeltmelisin
   app "YT-DLP Visualizer.app"
 
   depends_on formula: "yt-dlp"
